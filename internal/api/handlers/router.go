e86ef3a422290eedf72910c76bd3357c9401f388
dev.roberto.sousa <dev.roberto.sousa@gmail.com>
2025-03-07 14:56:37 -0300

    feat: Updating router

--- internal/api/handlers/router.go
+++ internal/api/handlers/router.go
@@ -1,11 +1,29 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/dev-roberto-sousa/nuvemhub/internal/api/config"
+	"github.com/dev-roberto-sousa/nuvemhub/internal/api/repositories"
+	"github.com/dev-roberto-sousa/nuvemhub/internal/api/services"
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/", HomeHandler)
+	config.ConnectDatabase()
+	db := config.DB
+
+	userRepo := repositories.NewUserRepository(db)
+	userService := services.NewUserService(userRepo)
+	userHandler := NewUserHandler(userService)
+
+	api := r.Group("/api/v1")
+	{
+		api.GET("/", HomeHandler)
+		api.GET("/users", userHandler.GetUsers)
+		api.POST("/users", userHandler.CreateUser)
+
+	}
 
 	return r
 }
