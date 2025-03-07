61153e04b0c332579869398da962235f808f743c
dev.roberto.sousa <dev.roberto.sousa@gmail.com>
2025-03-07 14:58:51 -0300

    feat: Moving file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/dev-roberto-sousa/nuvemhub/internal/api/config"
 	"github.com/dev-roberto-sousa/nuvemhub/internal/api/handlers"
 )
 
 func main() {
+	config.ConnectDatabase()
+
 	router := handlers.SetupRouter()
 
-	log.Println("Servidor rodando na porta 8080...")
+	log.Println("Server running on port 8080...")
 	err := router.Run(":8080")
 	if err != nil {
-		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+		log.Fatalf("Error starting server: %v", err)
 	}
 }
